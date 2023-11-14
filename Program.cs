78a69e1b8866bdba4f7508f184dc26aed2260f60
GabrieleCannavale <gabriele.cannavale92@gmail.com>
2023-11-14 16:27:28 +0100

    new csharp project (training booleans)

---
+++ Program.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using static System.Console;
+using static booleanOperators.Utils;
+
+namespace booleanOperators
+{
+    public class Program
+    {
+
+
+        static void Main(string[] args)
+        {
+            bool a = true;
+            bool b = false;
+
+            //log come stringa semplice
+            WriteLine($"AND  | a     | b    ");
+
+
+            WriteLine($"a    | {a & a,-5} | {a & b,-5} ");
+            WriteLine($"b    | {b & a,-5} | {b & b,-5} ");
+            WriteLine();
+
+            WriteLine($"OR   | a     | b    ");
+            WriteLine($"a    | {a | a,-5} | {a | b,-5} ");
+            WriteLine($"b    | {b | a,-5} | {b | b,-5} ");
+            WriteLine();
+            WriteLine($"XOR  | a     | b    ");
+            WriteLine($"a    | {a ^ a,-5} | {a ^ b,-5} ");
+            WriteLine($"b    | {b ^ a,-5} | {b ^ b,-5} ");
+
+            WriteLine($"a && DoStuff() = {a && DoStuff()}");
+            WriteLine($"b && DoStuff() = {b && DoStuff()}");
+
+
+
+            //operazioni con i booleani
+            WriteLine("inserisci un Nome");
+            string Nome = ReadLine();
+
+            if (ContieneValore(Nome))
+            {
+                WriteLine($"il nome è {Nome}");
+            }
+            else
+            {
+                WriteLine("IL VALORE DI NOME NON DEVE ESSERE VUOTO!");
+            }
+
+            WriteLine($"Nome && DoStuff() = {ContieneValore(Nome) && DoStuff()}");
+
+
+        }
+    }
+}
---
+++ Utils.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace booleanOperators
+{
+    public class Utils
+    {
+        public static bool ContieneValore(string NomeDaControllare)
+        {
+            if ((NomeDaControllare == null) || (NomeDaControllare == string.Empty))
+            {
+                return false;
+            }
+            return true;
+        }
+
+        public static bool DoStuff()
+        {
+            Console.WriteLine("I am doing some stuff.");
+            return true;
+        }
+    }
+}
